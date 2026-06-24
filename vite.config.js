3e2e1cf865ea7958d9849c4a662e835d7ecbf4c9
Tasnem Islam Prome <260295037+tasnem-tech@users.noreply.github.com>
2026-06-24 23:14:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite'
+export default defineConfig({
+  base: '/habitflow/'
+})
+  import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
